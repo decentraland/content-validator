e2d522aa734e27aafe96b88ada6940145b9b151e
guidota <guido@decentraland.org>
2022-01-03 16:35:13 -0300

    feat(tests): add 'rate-limit' validation tests

--- src/validations/rate-limit.ts
+++ src/validations/rate-limit.ts
@@ -5,7 +5,7 @@ import { conditionalValidation } from '../types'
  * @public
  */
 export const rateLimit = conditionalValidation({
-  predicate: async ({ deployment, externalCalls }) => await externalCalls.isEntityRateLimited(deployment.entity),
+  predicate: async ({ deployment, externalCalls }) => !(await externalCalls.isEntityRateLimited(deployment.entity)),
   message: ({ deployment }) =>
     `Entity rate limited (entityId=${deployment.entity.id} pointers=${deployment.entity.pointers.join(',')}).`,
 })
