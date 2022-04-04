3a68ed1c27d14dd1c52a67c3b8a963a3e5382349
Agustina Aldasoro <agusaldasoro@users.noreply.github.com>
2022-04-04 08:55:19 -0300

    Add a failure when deploying default scenes (#46)
    * Add a failure when deploying default scenes
    
    * add validation failed
    
    * Implement feedback

--- src/validations/access-checker/access.ts
+++ src/validations/access-checker/access.ts
@@ -1,11 +1,11 @@
-import { EntityType } from 'dcl-catalyst-commons'
+import { Entity, EntityType } from 'dcl-catalyst-commons'
 import { LEGACY_CONTENT_MIGRATION_TIMESTAMP } from '..'
-
-import { OK, Validation } from '../../types'
+import { OK, Validation, validationFailed } from '../../types'
 import { profiles } from './profiles'
 import { scenes } from './scenes'
 import { stores } from './stores'
 import { wearables } from './wearables'
+
 
 const accessCheckers: Record<EntityType, Validation> = {
   [EntityType.PROFILE]: profiles,
@@ -23,8 +23,20 @@ export const access: Validation = {
     const { deployment, externalCalls } = args
     const deployedBeforeDCLLaunch = deployment.entity.timestamp <= LEGACY_CONTENT_MIGRATION_TIMESTAMP
     const address = externalCalls.ownerAddress(deployment.auditInfo)
+
+    // Default scenes were removed from the Content Servers after https://github.com/decentraland/catalyst/issues/878
+    if (isDefaultScene(deployment.entity)) {
+      return validationFailed(
+        `Scene pointers should only contain two integers separated by a comma, for example (10,10) or (120,-45).`)
+    }
+    // Legacy entities still need to be synchronized
     if (deployedBeforeDCLLaunch && externalCalls.isAddressOwnedByDecentraland(address)) return OK
 
     return accessCheckers[deployment.entity.type].validate(args)
   },
+}
+
+function isDefaultScene(entity: Entity) {
+  return entity.type === EntityType.SCENE &&
+    entity.pointers.some(p => p.toLowerCase().startsWith('default'))
 }
