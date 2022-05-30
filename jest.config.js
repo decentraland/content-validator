cb2af0790576d4b5f7127d15bb57baccfdb49b82
Mariano Goldman <mariano.goldman@decentraland.org>
2022-05-30 12:43:02 -0300

    chore: tsconfig for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'test/tsconfig.json'
+    }
+  },
   coverageDirectory: 'coverage',
   verbose: true,
-  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: ['**/*.spec.(ts)'],
   testEnvironment: 'node',
 }
