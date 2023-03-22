2b6aad7167bb4e6d2ba730281406f1010ac17823
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2023-03-22 13:36:21 -0300

    test: for base wearable

--- test/unit/validations/metadata-schema.spec.ts
+++ test/unit/validations/metadata-schema.spec.ts
@@ -5,7 +5,7 @@ import { buildDeployment } from '../../setup/deployments'
 import { VALID_STANDARD_EMOTE_METADATA, VALID_THIRD_PARTY_EMOTE_METADATA_WITH_MERKLE_ROOT } from '../../setup/emotes'
 import { buildEntity } from '../../setup/entity'
 import { VALID_PROFILE_METADATA } from '../../setup/profiles'
-import { VALID_THIRD_PARTY_WEARABLE, VALID_WEARABLE_METADATA } from '../../setup/wearable'
+import { BASE_WEARABLE_METADATA, VALID_THIRD_PARTY_WEARABLE, VALID_WEARABLE_METADATA } from '../../setup/wearable'
 
 describe('Metadata Schema', () => {
   const POST_ADR_45_TIMESTAMP = ADR_45_TIMESTAMP + 1
@@ -75,6 +75,17 @@ describe('Metadata Schema', () => {
       type: EntityType.PROFILE,
       metadata: invalidMetadata,
       timestamp: PRE_ADR_45_TIMESTAMP
+    })
+    const deployment = buildDeployment({ entity })
+    const result = await metadataValidateFn(deployment)
+
+    expect(result.ok).toBeTruthy()
+  })
+
+  it('when deploying a base wearable the validation pass', async () => {
+    const entity = buildEntity({
+      type: EntityType.WEARABLE,
+      metadata: BASE_WEARABLE_METADATA
     })
     const deployment = buildDeployment({ entity })
     const result = await metadataValidateFn(deployment)
